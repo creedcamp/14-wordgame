f16e074e85ef0599865fe00067acf8e4493f22c0
Creed Camp <creedomed@gmail.com>
2020-09-22 13:34:20 -0700

    saving

---
+++ game.js
@@ -0,0 +1,29 @@
+//steps theorized
+//step 1: create a function to filter through commonWords and choose a
+//
+//
+//
+//
+//
+//
+//
+import commonWords from "./constants.js"
+const chooseWord = commonWords.filter(function (obj) {
+  if (obj[i] !== commonWords[0]) {
+    return obj[i]
+  } /*not sure if this else is needed */ else {
+    commonwords++
+  }
+  // let chosen = ""
+  // chosen = chosen + obj[i]
+  // console.log(chosen)
+  // return chosen
+})
+const makebar = function (readWords) {
+  let item = ""
+  if (item === undefined) {
+    return item + readWords
+  }
+}
+
+console.log(chooseWord)
